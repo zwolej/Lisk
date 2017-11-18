1cf111e83c74839ac4530f99ead4a3437bdbdea1
Bartosz Zwolinski <Bartosz.Zwolinski91@gmail.com>
2017-11-18 18:18:34 +0100

    Basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class  AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(){
+    particlesJS.load('particles-js', 'assets/particles.json', null);
+  }
 }
