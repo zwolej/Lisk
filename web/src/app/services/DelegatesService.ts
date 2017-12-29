6b3e577b5726eaaabb78320614e96d9d725a1765
Bartosz Zwoliński <bartosz.zwolinski@hl.co.uk>
2017-12-29 13:05:56 +0100

    Integration with backend.

---
+++ web/src/app/services/DelegatesService.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {WalletResponse} from "./model/WalletResponse";
+import {Router} from "@angular/router";
+import {VotedDelegateResponse} from "./model/VotedDelegateResponse";
+
+declare var lisk: any;
+
+@Injectable()
+export class LiskService {
+
+  private api: any;
+
+  constructor(private router: Router) {
+    const options = {
+      ssl: false,
+      node: '',
+      randomPeer: true,
+      testnet: true,
+      port: '7000',
+      bannedPeers: [],
+    };
+    this.api = lisk.api(options);
+  }
+
+  public getDelegates(callback: (n: any) => any): void {
+    this.api.listActiveDelegates(101, (data) => {
+      callback(data);
+    });
+  }
+
+  public getAccount(address: string, callback: (n: WalletResponse) => any): void {
+    this.api.getAccount(address, (data: WalletResponse) => {
+      callback(data);
+    });
+  }
+
+  public getAccountVotes(address: string, callback: (n: VotedDelegateResponse) => any): void {
+    this.api.listVotes(address, (data: VotedDelegateResponse) => {
+      callback(data);
+    });
+  }
+
+}
